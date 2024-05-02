4b68b970339ce71bb3ed9dbfbba3f97897fd1afd
Fabricio Sales <fabriciooliveirasales13@gmail.com>
2024-05-01 23:16:27 -0300

    Update java app

--- src/main/java/com/picpaysimplificado/PicpaysimplificadoApplication.java
+++ src/main/java/com/picpaysimplificado/PicpaysimplificadoApplication.java
@@ -2,8 +2,10 @@ package com.picpaysimplificado;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PicpaysimplificadoApplication {
 
 	public static void main(String[] args) {
